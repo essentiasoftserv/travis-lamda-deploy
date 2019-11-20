fb557214493585fa15978caa1e9d631017e63788
Abhishek <geekabhi@outlook.com>
2019-11-20 18:22:34 +0530

    added vue

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Lambda Test App</div>
+                      <br><br><br>
+                    <div class="card-body">
+                        <form @submit="formSubmit">
+                        <strong>Name:     </strong>
+                        <input type="text" class="form-control" v-model="name"><br><br><br>
+                        <strong>Description:  </strong>
+                        <textarea class="form-control" v-model="description"></textarea><br><br><br>
+    
+                        <button class="btn btn-success">Submit</button>
+                        </form>
+                        <br><br><br>
+                        <strong>Output:</strong>
+                        <pre>
+                        {{output}}
+                        </pre>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+     
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        },
+        data() {
+            return {
+              name: '',
+              description: '',
+              output: ''
+            };
+        },
+        methods: {
+            formSubmit(e) {
+                e.preventDefault();
+                let currentObj = this;
+                this.axios.post('https://hb3gb6lzd4.execute-api.us-east-1.amazonaws.com/newstage/newdeploy', {
+                    name: this.name,
+                    // description: this.description
+                },
+                {
+                  headers: {
+                    ContentType: "application/x-www-form-urlencoded",
+                    Accept: "*/*",
+                    AccessControlAllowOrigin: "*", 
+                    AccessControlAllowCredentials: true 
+          
+                    }
+
+                    /*
+                    headers: {
+                      "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+                      "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+                    },
+                    */
+                }
+                )
+                .then(function (response) {
+                    currentObj.output = response.data;
+                })
+                .catch(function (error) {
+                    currentObj.output = error;
+                    // console.log(error);
+                });
+            }
+        }
+    }
+</script>
---
+++ frontend/src/components/Ping.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>
+      <br>
+        <b>{{ msg }}</b>
+    </p>
+    <img src="../assets/emoji.png" style="height: 90px;width=90px;" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Ping",
+  data() {
+    return {
+      msg: "Do you mind looking here ?"
+    };
+  }
+};
+</script>
+
+<style>
+img {
+  size: 10em;
+}
+</style>
